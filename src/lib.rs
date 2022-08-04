2942dfbd05478171a474421a00521d13f4baedd4
Nathaniel Curnick <nathaniel.curnick@gmail.com>
2022-08-04 20:07:36 +0100

    chore: :fire: Removed all basic enums and types to yahoo-finance crate for ease of use

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,7 @@
 //! # Market Finance
-//! 
+//!
 //! A collection of data structures, algorithms and functions for use with a variety of financial markets.
 //!
-//! Technical indicators are currently beta and not enabled by default.
+//! Technical indicators are enabled but still beta - use at own risk
 
-mod traits;
-pub use traits::{ Timestamped };
-
-#[cfg(feature = "indicators")]
 pub mod indicators;
-
-mod interval;
-pub use interval::Interval;
-
-mod bar;
-pub use bar::Bar;
-
-mod quote;
-pub use quote::{ Quote, TradingSession };
